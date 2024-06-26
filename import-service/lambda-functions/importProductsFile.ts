542c6946f4be20b4e7bd056d2af566443f849e7d
Екатерина <ekaterinahubkina@yandex.by>
2024-06-27 00:24:38 +0300

    create import service

---
+++ import-service/lambda-functions/importProductsFile.ts
@@ -0,0 +1,38 @@
+import { APIGatewayEvent, S3Event } from "aws-lambda";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { createResponse } from "../utils/create-response";
+
+const BUCKET_NAME = process.env.BUCKET_NAME || "";
+
+export async function handler(event: APIGatewayEvent) {
+  console.log(
+    "Import products file handler incoming request",
+    event.queryStringParameters?.name
+  );
+
+  const name = event.queryStringParameters?.name;
+
+  if (!name) {
+    return createResponse({
+      statusCode: 400,
+      body: { message: "Name parameter is missing" },
+    });
+  }
+
+  const client = new S3Client();
+  const command = new PutObjectCommand({ Bucket: BUCKET_NAME, Key: name });
+
+  try {
+    const presignedUrl = await getSignedUrl(client, command, {
+      expiresIn: 3600,
+    });
+
+    return createResponse({
+      statusCode: 200,
+      body: { url: presignedUrl },
+    });
+  } catch (error) {
+    return createResponse({ statusCode: 500, body: error });
+  }
+}
---
+++ import-service/lib/import-service-stack.ts
@@ -0,0 +1,61 @@
+import * as cdk from "aws-cdk-lib";
+import { Cors, LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import {
+  Function as LambdaFunction,
+  Runtime,
+  Code,
+  FunctionProps,
+} from "aws-cdk-lib/aws-lambda";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+import path = require("path");
+
+export class ImportServiceStackKate extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const BUCKET_NAME = process.env.BUCKET_NAME ?? "";
+
+    const importServiceBucket = Bucket.fromBucketName(
+      this,
+      "importServiceBucketKate",
+      BUCKET_NAME
+    );
+
+    const lambdaFunctionProps: Omit<FunctionProps, "handler"> = {
+      runtime: Runtime.NODEJS_20_X,
+      code: Code.fromAsset(path.join(__dirname + "/../lambda-functions")),
+      environment: { BUCKET_NAME },
+    };
+
+    const importProductsFile = new LambdaFunction(
+      this,
+      "ImportProductsFileHandlerKate",
+      {
+        handler: "importProductsFile.handler",
+        ...lambdaFunctionProps,
+      }
+    );
+
+    importServiceBucket.grantReadWrite(importProductsFile);
+
+    const api = new RestApi(this, "ImportServiceKate", {
+      restApiName: "ImportServiceKate",
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+      },
+    });
+
+    const importPath = api.root.addResource("import");
+
+    importPath.addMethod("GET", new LambdaIntegration(importProductsFile), {
+      requestParameters: {
+        "method.request.querystring.name": true,
+      },
+      requestValidatorOptions: {
+        validateRequestParameters: true,
+      },
+    });
+  }
+}
